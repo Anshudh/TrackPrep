660c158414806ed53074ed7155ab37789ea3260a
Anshu <24bcs022@nith.ac.in>
2026-06-23 18:18:47 +0530

    resolve proxy traget

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const backendTarget = process.env.BACKEND_URL || 'http://localhost:5000';
+const backendTarget = process.env.BACKEND_URL || process.env.VITE_BACKEND_URL || 'http://localhost:5000';
 
 // https://vitejs.dev/config/
 export default defineConfig({
